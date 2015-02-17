1e5f5c21d498ca6702ec6fd3f0d98b95bbf870e3
Li Hao <ash.lihao@gmail.com>
2015-02-17 23:23:59 +0800

    pull and plot pair script:

--- pullData.py
+++ pullData.py
@@ -19,8 +19,10 @@ ts = stock.get_historical('2014-01-01',today.strftime("%Y-%m-%d"))
 filew = open('db/' + args.code + '.txt', 'w')
 
 # save to file in OHLC format
-for eachDay in ts:
-   filew.write(eachDay['Date'] + ',' + eachDay['Open'] + ',' + eachDay['High'] + "," + eachDay['Low'] + ',' + eachDay['Close'] + "\n")
+for i in range(1, len(ts)+1):
+   eachDay = ts[-i]
+   
+   filew.write(eachDay['Date'] + ',' + eachDay['Open'] + ',' + eachDay['High'] + "," + eachDay['Low'] + ',' + eachDay['Close'] + "," + eachDay['Volume'] + "\n")
 
 filew.close()
 print "done"
