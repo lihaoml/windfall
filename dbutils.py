796789c681c0495652658bde6253fe84b6d8257a
Li Hao <ash.lihao@gmail.com>
2017-01-15 02:13:26 +0800

    integrate google finance in dbutils

--- dbutils.py
+++ dbutils.py
@@ -4,9 +4,11 @@ from yahoo_finance import Share
 
 import argparse
 import datetime, time
+from datetime import timedelta
 
 import sqlite3 as lite
 import pandas
+from google_finance import google_hist_price
 
 class MarketDataStore:
    """ Storing and retrieving market data from database (SQLite)
@@ -15,7 +17,9 @@ class MarketDataStore:
    def __init__(self, db_path):
       self.con = lite.connect(db_path)
       self.cur = self.con.cursor()
-      self.hist_data_tablename = 'HIST_DATA'
+      # self.hist_data_tablename = 'HIST_DATA'
+      self.hist_data_yahoo_tablename = 'HIST_DATA_YAHOO'
+      self.hist_data_google_tablename = 'HIST_DATA_GOOGLE'
       self.stock_tablename = 'STOCK'
 
    def __del__(self):
@@ -27,36 +31,50 @@ class MarketDataStore:
       today = datetime.date.today()
       for symbol in stock_sym_list:
          print ("pulling " + symbol)
+
+         ### retrieve data from yahoo ################
          stock = Share(symbol)
-         last_date = self.get_last_date(symbol)
+         last_date = self.get_last_date(self.hist_data_yahoo_tablename, symbol)
+         print last_date
          # filter out the strings in the returned list
-         ts_ = [t for t in stock.get_historical(last_date.strftime("%Y-%m-%d"),today.strftime("%Y-%m-%d")) if type(t) != type("")]
+         ts_ = [t for t in stock.get_historical(last_date.strftime("%Y-%m-%d"), today.strftime("%Y-%m-%d")) if type(t) != type("")]
          ts = [t for t in ts_ if t['Date'] == t['Date']] # check that date is not nan
          # when there is no data to retrieve, get_historical returns a list of strings where each string is the column name
          # when there is data, get_historical returns a list of dict
          if len(ts) > 0:
             df = pandas.DataFrame(ts).set_index(['Symbol', 'Date'], drop=True, append=False, verify_integrity = True)
             print df
-            df.to_sql(self.hist_data_tablename, self.con, schema=None, if_exists='append', index=True, index_label=['Symbol', 'Date'])
+            df.to_sql(self.hist_data_yahoo_tablename, self.con, schema=None, if_exists='append', index=True, index_label=['Symbol', 'Date'])
          else:
-            print "%s: no new data to be added from %s to %s"%(symbol, last_date, today)
+            print "Yahoo %s: no new data to be added from %s to %s"%(symbol, last_date, today)
+
+         ### retrieve data from google ###############
+         last_date = self.get_last_date(self.hist_data_google_tablename, symbol)
+         df2 = google_hist_price(symbol, (last_date+timedelta(1)).strftime("%Y-%m-%d"), today.strftime("%Y-%m-%d"))
+         if not(df2.empty):
+            print df2
+            df2.to_sql(self.hist_data_google_tablename, self.con, schema=None, if_exists='append', index=True, index_label=['Symbol', 'Date'])
+         else:
+            print "Google %s: no new data to be added from %s to %s"%(symbol, last_date, today)
+
+         time.sleep(5)
          
-   def get_last_date(self, symbol):
+   def get_last_date(self, tablename, symbol):
       try:
-         command_str = "SELECT DATE FROM %s where symbol = '%s'"%(self.hist_data_tablename, symbol)
+         command_str = "SELECT DATE FROM %s where symbol = '%s'"%(tablename, symbol)
          self.cur.execute(command_str)
          rows = self.cur.fetchall()
          return datetime.datetime.strptime(max([n[0].encode() for n in rows]), "%Y-%m-%d").date()
       except:
          return datetime.date(1990, 1, 1)
 
-   def get_hist_data(self, symbol):
-      command_str = "SELECT * FROM %s where symbol = '%s'"%(self.hist_data_tablename, symbol)
+   def get_hist_data(self, tablename, symbol):
+      command_str = "SELECT * FROM %s where symbol = '%s'"%(tablename, symbol)
       return pandas.read_sql_query(sql = command_str, con = self.con, index_col = ['Date'], parse_dates = ['Date'])
 
-   def get_symbols_from_exchange(self, table, exchange):
+   def get_symbols_from_exchange(self, exchange):
       try:
-         command_str = "SELECT SYMBOL FROM %s where EXCHANGE = '%s'"%(table, exchange)
+         command_str = "SELECT SYMBOL FROM %s where EXCHANGE = '%s'"%(self.stock_tablename, exchange)
          self.cur.execute(command_str)
          rows = self.cur.fetchall()
          return sorted([n[0].encode() for n in rows])
@@ -70,7 +88,7 @@ if __name__ == "__main__":
 
    args = parser.parse_args()
    mds = MarketDataStore("db/mds.sqlite")
-   symbols = mds.get_symbols_from_exchange(mds.stock_tablename, args.exchange)
+   symbols = mds.get_symbols_from_exchange(args.exchange)
    print "============Start pulling market data==============="
    print symbols
    print len(symbols)
--- google_finance.py
+++ google_finance.py
@@ -1,22 +1,3 @@
-# Copyright (c) 2011, Mark Chenoweth
-# All rights reserved.
-#
-# Redistribution and use in source and binary forms, with or without modification, are permitted 
-# provided that the following conditions are met:
-#
-# - Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
-#
-# - Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following 
-#   disclaimer in the documentation and/or other materials provided with the distribution.
-#
-# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, 
-# INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE 
-# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
-# EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS 
-# OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, 
-# STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF 
-# ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-
 import urllib,time,datetime
 import urllib
 from urllib2 import urlopen
@@ -24,9 +5,10 @@ from bs4 import BeautifulSoup
 import requests
 from datetime import datetime,date,timedelta
 import pandas as pd
+import time
 
-def google_hist_price(symbol,start_date, end_date=date.today().isoformat()):
-  symbol = symbol.upper()
+def google_hist_price(symbol_input, start_date, end_date=date.today().isoformat()):
+  symbol_to_query = symbol_input.upper().split(".")[0]
   df = pd.DataFrame()
   start = date(int(start_date[0:4]),int(start_date[5:7]),int(start_date[8:10]))
   end = date(int(end_date[0:4]),int(end_date[5:7]),int(end_date[8:10]))
@@ -37,31 +19,45 @@ def google_hist_price(symbol,start_date, end_date=date.today().isoformat()):
     date_list.append( ( start+i*timedelta(delta), min(end, start+(i+1)*timedelta(delta)-timedelta(1)) )  )
   
   for (d1, d2) in date_list:
-    url_string = "http://www.google.com/finance/historical?q={0}".format(symbol)
+    url_string = "http://www.google.com/finance/historical?q={0}".format(symbol_to_query)
     url_string += "&num=200&startdate={0}&enddate={1}".format(d1.strftime('%b %d, %Y'), d2.strftime('%b %d, %Y'))
     print url_string
-
+    time.sleep(1)
     page2 = urllib.urlopen(url_string).read()
     soup = BeautifulSoup(page2, "lxml")
     
     headers = [h.string.rstrip().encode() for h in soup.findAll("th")]
     dct = {key: [] for key in headers}
+
+    if (not(any(dct))):
+      continue
+
     for row in soup.find_all('tr')[5:]:
       tds = [d.string.rstrip().encode() for d in row.find_all('td')]
       for (h, d) in zip(headers, tds):
         dct[h].append(d)
-        
-    dct['Date'] = [datetime.strptime(d, "%b %d, %Y").date() for d in dct['Date']]
-    dct['Volume'] = [int(d.replace(",", "")) for d in dct['Volume']]
-    dct['Open'] = [float(d.replace(",", "")) for d in dct['Open']]
-    dct['Close'] = [float(d.replace(",", "")) for d in dct['Close']]
-    dct['High'] = [float(d.replace(",", "")) for d in dct['High']]
-    dct['Low'] = [float(d.replace(",", "")) for d in dct['Low']]
 
+    def safeConvert(f, x):
+      try:
+        return f(x)
+      except:
+        return f('nan')
+    
+    dct['Date'] = [datetime.strptime(d, "%b %d, %Y").date().strftime("%Y-%m-%d") for d in dct['Date']]
+    dct['Volume'] = [safeConvert(int, d.replace(",", "")) for d in dct['Volume']]
+    dct['Open'] = [safeConvert(float, d.replace(",", "")) for d in dct['Open']]
+    dct['Close'] = [safeConvert(float, d.replace(",", "")) for d in dct['Close']]
+    dct['High'] = [safeConvert(float, d.replace(",", "")) for d in dct['High']]
+    dct['Low'] = [safeConvert(float, d.replace(",", "")) for d in dct['Low']]
+      
     if df.empty:
-      df = pd.DataFrame(dct).set_index('Date')
+      df = pd.DataFrame(dct)
     else:
-      df = df.append(pd.DataFrame(dct).set_index('Date'))
+      df = df.append(pd.DataFrame(dct))
+
+  if not(df.empty):
+    df['Symbol'] = symbol_input
+    df = df.set_index(['Symbol', 'Date'], drop=True, append=False, verify_integrity = True).sort_index()
   return df
     
 def google_hist_price_csv(symbol,start_date,end_date=date.today().isoformat()):
